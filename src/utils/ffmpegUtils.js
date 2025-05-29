a5a934357ebba8e78162502bc43b7109fff6633e
duochq77 <duochq77@gmail.com>
2025-05-29 17:38:04 +0700

    � Remove upload-job.ts and clean dist

--- src/utils/ffmpegUtils.js
+++
@@ -1,7 +0,0 @@
-/**
- * Tạo câu lệnh FFmpeg để ghép file video và audio
- * Dùng cho worker hoặc xử lý local
- */
-export function generateFFmpegCommand(inputVideo, inputAudio, outputPath) {
-    return `ffmpeg -i ${inputVideo} -i ${inputAudio} -c:v copy -c:a aac -shortest ${outputPath}`;
-}
