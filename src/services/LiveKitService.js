c09a17b33e9109fc448fff265ec058b978bea884
duochq77 <duochq77@gmail.com>
2025-05-29 17:38:04 +0700

    � Remove upload-job.ts and clean dist

--- src/services/LiveKitService.js
+++
@@ -1,7 +0,0 @@
-const livekit = require('livekit-client');
-// Khởi tạo kết nối và trả về room
-export async function connectToRoom(serverUrl, token) {
-    const room = new livekit.Room();
-    await room.connect(serverUrl, token);
-    return room;
-}
